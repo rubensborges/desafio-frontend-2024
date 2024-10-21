a9361752e3a23632367ca0981120726c2ce90bdc
Rubens Borges <contact@rubensborges.dev>
2024-10-20 22:43:57 -0300

    fix: declared standalone component

--- desafio-inovamobil/src/core/components/pokemon-list/pokemon-list.component.ts
+++ desafio-inovamobil/src/core/components/pokemon-list/pokemon-list.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { PokemonCardComponent } from '../pokemon-card/pokemon-card.component';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.css']
+  styleUrls: ['./pokemon-list.component.scss'],
+  imports: [PokemonCardComponent],
+  standalone: true,
 })
-export class PokemonListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class PokemonListComponent {}
