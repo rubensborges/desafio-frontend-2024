ac32f017205ba5a786dd3f46d747e42cd120dc5b
Rubens Borges <contact@rubensborges.dev>
2024-10-21 21:44:24 -0300

    feat: added api call for pokemon service

--- desafio-inovamobil/src/services/pokemon/pokemon-service.service.ts
+++ desafio-inovamobil/src/services/pokemon/pokemon-service.service.ts
@@ -1,10 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonServiceService {
 
-constructor() { }
+  private url: string = 'https://pokeapi.co/api/v2/type/'
+constructor(private http: HttpClient) { }
+
+getPokemon(type: string): Observable<any>{
+  const newURl = `${this.url}${type}`
+  return this.http.get(newURl)
+}
 
 }
