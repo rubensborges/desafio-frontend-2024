2a293f19ed0d84782bd76c9c8148e20fd5f8a080
Rubens Borges <contact@rubensborges.dev>
2024-10-22 15:32:54 -0300

    feat: created fetch rule for pokemon image

--- desafio-inovamobil/src/services/pokemon-image/pokemon-image.service.ts
+++ desafio-inovamobil/src/services/pokemon-image/pokemon-image.service.ts
@@ -1,10 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonImageService {
 
-constructor() { }
+constructor(private http: HttpClient) { }
 
+  private baseUrl: string = 'https://pokeapi.co/api/v2/pokemon/'
+
+  getImageByName(name: string): Observable<any>{
+    const newUrl = `${this.baseUrl}${name}`
+    return this.http.get(newUrl)
+  }
 }
