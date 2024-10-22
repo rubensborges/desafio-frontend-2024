eeecadf0274c01793901972accd2dfe9af038196
Rubens Borges <contact@rubensborges.dev>
2024-10-21 21:49:08 -0300

    feat: created new variables for the data consuming

--- desafio-inovamobil/src/components/pokemon-card/pokemon-card.component.ts
+++ desafio-inovamobil/src/components/pokemon-card/pokemon-card.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { WeatherServiceService } from '../../services/weather/weather-service.service';
+import { PokemonServiceService } from '../../services/pokemon/pokemon-service.service';
 
 @Component({
   selector: 'app-pokemon-card',
@@ -6,4 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./pokemon-card.component.scss'],
   standalone: true,
 })
-export class PokemonCardComponent {}
+export class PokemonCardComponent {
+  city: string = ''
+  weather: string = ''
+  pokemonName: string = ''
+  pokemonType: string = ''
+  isRaining: boolean = false
+
+  constructor(private weatherService: WeatherServiceService, private pokemonService: PokemonServiceService) {}
+
+}
