89b173d3dc69d86056ef5ff65cf925925e549715
Rubens Borges <contact@rubensborges.dev>
2024-10-22 11:01:31 -0300

    feat: added new rule for getting the nema typed on input

--- desafio-inovamobil/src/app/app.component.ts
+++ desafio-inovamobil/src/app/app.component.ts
@@ -2,16 +2,27 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { PokemonCardComponent } from "../components/pokemon-card/pokemon-card.component";
+import { ShareCityNameService } from '../services/share/share-city-name.service';
+import { FormsModule } from '@angular/forms';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatIconModule, PokemonCardComponent],
+  imports: [RouterOutlet, FormsModule, MatIconModule, PokemonCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'desafio-inovamobil';
+  
+  city: string = ''
+  
+  constructor(private shareCityName: ShareCityNameService){}
+
+  onSubmit(){
+    this.shareCityName.setCity(this.city)
+  }
+
 }
