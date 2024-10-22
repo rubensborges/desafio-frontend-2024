2ed0b3c97db7905d5b0858ef005ccbe11af1311a
Rubens Borges <contact@rubensborges.dev>
2024-10-21 21:41:00 -0300

    feat: added api call for weather service

--- desafio-inovamobil/src/services/weather/weather-service.service.ts
+++ desafio-inovamobil/src/services/weather/weather-service.service.ts
@@ -1,10 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherServiceService {
 
-constructor() { }
+  private apiKey: string =  '5723764d68edb7f764d7be3aa599977f'
+  private url: string =  'https://api.openweathermap.org/data/2.5/'
+
+constructor(private http: HttpClient) {}
+  getWather(city: string): Observable<any>{
+    const newURl = `${this.url}?q${city}&appid=${this.apiKey}`
+    return this.http.get(newURl)
+  }
 
 }
