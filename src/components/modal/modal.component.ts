96d868644e375560f3afa7b889d0a1cde23ffb2a
Rubens Borges <contact@rubensborges.dev>
2024-10-23 12:51:05 -0300

    feat: modal error message finished

--- src/components/modal/modal.component.ts
+++ src/components/modal/modal.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss'],
+  imports: [MatIconModule],
   standalone: true
 })
 export class ModalComponent {
